ab503d3dc556482de77883fe07c3064d00e873e2
Illia Aprol <asdf21072107@gmail.com>
2020-04-27 20:33:11 +0200

    Added buttons
    Rewritten implementation of buttons, added some functions to check if a point is near another point.

--- sketch.js
+++ sketch.js
@@ -1,31 +1,29 @@
 const fWidth = 20, fHeight = 20, fieldSize = 30, offsetX = 50, offsetY = 50, fAlpha = 100;
-const f = []; // 0 - uncaptured; 1 - player 1; 2 - player 2; ...
+const f = []; // -1 - uncaptured; 0 - player 0; 1 - player 1; ...
 const ter = []; // 0 - ground; 1 - mountain; 2 - sea
 const unit = []; // man; tank; city; wall
 const player = []; // active palyers
+const buttons = [];
+let chosenUnit, action, activePlayer = 0;
 
-class ClickMgr {
-  constructor() {
-    this.zones = [];
-  }
-
-  addZone(x, y, w, h, callback, priority = 0, group = 0) {
-    this.zones.push({
-      x, y, w, h, callback, priority, group,
+class Button {
+  constructor(x, y, w, h, callback, scalable = false, col = undefined, text = '', priority = 0, group = 0) {
+    buttons.push({
+      x, y, w, h, callback, priority, group, scalable, col, text,
     });
   }
 
   delGroup(n) {
-    this.zones.forEach(z => {
-      if (z.group === n) {
-        this.zones.splice(this.zones.indexOf(z));
+    buttons.forEach(b => {
+      if (b.group === n) {
+        buttons.splice(this.zones.indexOf(z));
       }
     });
   }
 
-  onClick() {
+  static onClick() {
     const act = [];
-    this.zones.forEach((zone) => {
+    buttons.forEach((zone) => {
       if (isInArea(mouseX, mouseY, zone.x, zone.y, zone.w, zone.h)) {
         act.push(zone);
       }
@@ -39,30 +37,76 @@ class ClickMgr {
 }
 
 class Unit {
-  constructor(i, j, maxHp, hp = maxHp) {
+  constructor(i, j, plr, maxHp, hp = maxHp) {
     this.i = i;
     this.j = j;
+    this.plr = plr;
     this.hp = hp;
     this.maxHp = maxHp;
+  }
+
+  static getUnit(i, j) {
+    let res;
+    unit.forEach(u => {
+      if (u.i === i && u.j === j) {
+        res = u;
+      }
+    });
+    return res;
   }
 }
 
 class City extends Unit {
-  constructor(i, j) {
-    super(i, j, 8);
+  constructor(i, j, plr) {
+    super(i, j, plr, 8);
     this.level = 0;
+    f[i][j] = plr.id;
+    setAdj(i, j, plr.id);
   }
 }
 
 class Man extends Unit {
-  constructor(i, j) {
-    super(i, j, 8);
+  constructor(i, j, plr) {
+    super(i, j, plr, 8);
   }
+}
+
+function setAdj(i, j, val, ifClear = true) {
+  for (let p = -1; p <= 1; p++) {
+    for (let q = -1; q <= 1; q++) {
+      if (f[i + p] !== undefined && f[i + p][j + q] !== undefined && (!ifClear || f[i + p][j + q] === -1)) {
+        f[i + p][j + q] = val;
+      }
+    }
+  }
+}
+
+function isAdj(i0, j0, i, j, d = 0, noCenter = true) { // d: 0 - cross [4]; 1 - square [8] (setAdj); 2 - phombus [12]
+  if (noCenter && i === i0 && j === j0) {
+    return false;
+  }
+  if (d === 0) {
+    if ((Math.abs(i0 - i) <= 1 && Math.abs(j0 - j) === 0) || (Math.abs(j0 - j) <= 1 && Math.abs(i0 - i) === 0)) {
+      return true;
+    }
+  } else if (d === 1) {
+    if (Math.abs(i0 - i) <= 1 && Math.abs(j0 - j) <= 1) {
+      return true;
+    }
+  } else if (d === 2) {
+    if (((Math.abs(i0 - i) <= 2 && Math.abs(j0 - j) === 0) || (Math.abs(j0 - j) <= 2 && Math.abs(i0 - i) === 0))
+    || (Math.abs(i0 - i) <= 1 && Math.abs(j0 - j) <= 1)) {
+      return true;
+    }
+  } else {
+    throw new Error('isAdj: invalid argument "d"');
+  }
+  return false;
 }
 
 function getType(u) {
   if (!(u instanceof Unit)) {
-    throw new Error('getType works for Units only!');
+    return undefined;
   }
   if (u instanceof City) {
     return 'City';
@@ -83,38 +127,76 @@ class Player {
   conq(i, j) {
     f[i][j] = this.id;
   }
+
+  static nextTurn() {
+    chosenUnit = undefined;
+    action = undefined;
+    activePlayer = activePlayer === player.length - 1 ? 0 : ++activePlayer;
+    console.log(activePlayer);
+  }
 }
 
-const click = new ClickMgr();
 const clickEvent = () => {
-  click.onClick();
+  Button.onClick();
 };
 
+function fieldClick(i, j) {
+  console.log(i, j);
+  const u = Unit.getUnit(i, j);
+  console.log(u);
+  if (getType(u) === 'City' && u.plr.id === activePlayer) {
+    chosenUnit = u;
+    showCityButtons(u);
+  }
+}
 
 // eslint-disable-next-line no-unused-vars
 function setup() {
   player.push(new Player(0, color(0, 0, 255, fAlpha)));
   player.push(new Player(1, color(255, 0, 0, fAlpha)));
   initArr(f, -1);
+  initArr(ter);
+  unit.push(new City(0, 0, player[0]));
+  unit.push(new City(fWidth - 1, fHeight - 1, player[1]));
   player[0].conq(0, 0);
   player[1].conq(fWidth - 1, fHeight - 1);
-  initArr(ter);
-  console.log(f);
+  // console.log(f);
   genTerrain();
   initGridClick();
-  const cnv = createCanvas(800, 800);
+  const cnv = createCanvas(1000, 700);
   cnv.mouseClicked(clickEvent);
+  showMainButtons();
 }
 
 
 // eslint-disable-next-line no-unused-vars
 function draw() {
   background(0);
+  drawButtons();
   drawBackgr();
   drawBorders();
   drawUnits();
 }
 
+function showMainButtons() {
+  buttons.push(new Button(width - 250, height - 100, 200, 50, nextTurn, false, color(255), 'NEXT TURN', 1, 1));
+}
+
+function nextTurn() {
+  Player.nextTurn();
+}
+
+function showCityButtons(u) {
+  buttons.push(new Button(width - 200, 100, 100, 50, lock(buyMan, u), false, color(255), 'man', 1, 2));
+}
+
+function hideCityButtons() {
+  Button.delGroup(2);
+}
+
+function buyMan(u) {
+  
+}
 
 function getXYofFied(x, y) {
   return [x * fieldSize + offsetX, y * fieldSize + offsetY];
@@ -124,14 +206,10 @@ function initGridClick() {
   for (let i = 0; i < fWidth; i++) {
     for (let j = 0; j < fHeight; j++) {
       const [x, y] = getXYofFied(i, j);
-      click.addZone(x, y, fieldSize, fieldSize, lock(fieldClick, i, j));
+      buttons.push(new Button(x, y, fieldSize, fieldSize, lock(fieldClick, i, j), true));
     }
   }
-  console.log(click.zones);
-}
-
-function fieldClick(i, j) {
-  console.log(i, j);
+  console.log(buttons);
 }
 
 const lock = (f, ...args) => () => f(...args);
@@ -199,6 +277,23 @@ function drawBackgr() {
   }
 }
 
+function drawButtons() {
+  buttons.forEach(b => {
+    if (b.col !== undefined) {
+      drawButton(b);
+    }
+  });
+}
+
+function drawButton(b) {
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  fill(b.col);
+  rect(b.x, b.y, b.w, b.h);
+  fill(0);
+  text(b.text, b.x, b.y, b.w, b.h);
+}
+
 function drawF(x, y, ...col) {
   fill(...col);
   rect(x, y, fieldSize, fieldSize);
@@ -218,12 +313,12 @@ function drawSea(x, y) {
   drawF(x, y, 130, 160, 255);
 }
 
-function drawCity(x, y) {
+function drawCity(x, y, plrNum) {
   fill(100);
   rect(x + fieldSize / 4, y + fieldSize / 5, fieldSize / 2, (fieldSize * 4) / 5);
 }
 
-function drawMan(x, y) {
+function drawMan(x, y, plrNum) {
   fill(50);
   ellipse(x + fieldSize / 2, y + fieldSize / 2, fieldSize / 2, fieldSize / 2);
 }
@@ -243,9 +338,9 @@ function drawBorders() {
 function drawUnit(u) {
   const [x, y] = getXYofFied(u.i, u.j);
   if (getType(u) === 'City') {
-    drawCity(x, y);
+    drawCity(x, y, u.plr);
   } else if (getType(u) === 'Man') {
-    drawMan(x, y);
+    drawMan(x, y, u.plr);
   }
 }
 
